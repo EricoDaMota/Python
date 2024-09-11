af8245f05129221719bb8195c5081609f5505116
EricoDaMota <eriquinhofreire@gmail.com>
2024-09-11 14:29:32 -0300

    Add files via upload

---
+++ aula 01#.py
@@ -0,0 +1,17 @@
+print("Erico")
+print('Erico')
+print('Erico da Mota "Quinho" Freire')
+print("Erico da Mota \"Quinho\" Freire")
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17 
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = false
+print(teste)
+print(type(teste))

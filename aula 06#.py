b53974c387e9e9679c41d9e11170b8464ae31881
EricoDaMota <eriquinhofreire@gmail.com>
2024-09-12 17:22:07 -0300

    Update aula 06#.py

--- aula 06#.py
+++ aula 06#.py
@@ -4,6 +4,8 @@ print(30 * '~')
 idade = int(input('Digite a sua idade: '))
 if idade <= 15:
     print('Voto recusado')
+elif idade > 113:
+    print('A sua idade esta fora do intervlo esperado')
 elif idade >= 16 and idade < 18 or idade >= 65:
     print('O seu voto é facultativo')
 elif idade >= 18:

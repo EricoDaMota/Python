a6cc7f19917dc9d8bc1e05124aa527e4bc5c3371
EricoDaMota <eriquinhofreire@gmail.com>
2024-09-25 17:11:32 -0300

    Update aula 10#.py

--- aula 10#.py
+++ aula 10#.py
@@ -2,4 +2,4 @@ ciga = int(input('Digite quantos cigarros você fuma por dia: '))
 anos = int(input('Quantos anos você fuma: '))
 fm = anos * 365 * (ciga * 10)
 dias = (fm / 60) / 24
-print(f'Você perdeu {dias} de vida')
+print(f'Você perdeu {dias:.2f} de vida')
